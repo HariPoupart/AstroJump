979253b5098a588d2c973f11558ecfc909ff988b
harip <haripoupart@gmail.com>
2025-03-09 22:31:47 -0400

    making background even easier to evelyne

--- src/main/java/com/example/astrojumppseudocode/Background.java
+++ src/main/java/com/example/astrojumppseudocode/Background.java
@@ -1,14 +1,19 @@
 package com.example.astrojumppseudocode;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
 
 public class Background extends SimpleMovingImage {
     //general info
     private final int TOTAL_WIDTH;
+    private final int INITIAL_PLANET_INDEX;
 
-
-    Background(ImageView img,int width, int height,float speedX,int totalWidth){
+    Background(ImageView img,int width, int height,float speedX,int totalWidth,int planetIndex){
         super(img,width,height,speedX,0);
         this.TOTAL_WIDTH = totalWidth;
+        this.INITIAL_PLANET_INDEX = planetIndex;
+
+        //set viewport
+        changePlanet(INITIAL_PLANET_INDEX);
     }
 
     public boolean isHalfway(){
@@ -26,5 +31,10 @@ public class Background extends SimpleMovingImage {
         if(this.getX()<=TOTAL_WIDTH*-0.5)
             this.setX(0);
     }
+    public void changePlanet(int planetIndex){
+        //set the viewpoint to a rectangle situated at the planet's index
+        this.getImage().setViewport(new Rectangle2D(this.getWidth(),this.getHeight()*planetIndex,this.getWidth(),this.getHeight()));
+    }
+
 
 }
