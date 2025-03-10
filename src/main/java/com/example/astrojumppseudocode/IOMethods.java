c6b4ff9bdc01982c7e3299b1350a2781d92165aa
luinsoo <evepigeon100@hotmail.com>
2025-03-10 10:06:47 -0400

    start of io class

---
+++ src/main/java/com/example/astrojumppseudocode/IOMethods.java
@@ -0,0 +1,29 @@
+package com.example.astrojumppseudocode;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+
+public class IOMethods {
+
+    //attributes
+    public int highScore;
+    public int totalStarsCollected;
+
+    //constructors
+
+
+    //accessor and mutator methods
+    public int gethighScore() {
+        return highScore;
+    }
+    public int getTotalStarsCollected() {
+        return totalStarsCollected;
+    }
+
+    public void setHighScore(int newHighScore) {
+        //DataInputStream input = new InputStream("highScore.txt");
+    }
+    public void setTotalStarsCollected(int newStarsCollected) {
+
+    }
+}
